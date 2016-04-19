1c62da6db39480adb536683fa9f06c5d7060f357
moranpatrick <moran.patrick@gmail.com>
2016-04-19 22:50:05 +0100

    Add A Task

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,24 @@
 angular.module('starter.controllers', [])
 
-.controller('Tasks',function($scope) {
+
+.controller('Tasks',function($scope, Task) {
+    $scope.data = Task.data;
     
+     function deleteTask() {
+        Task.deleteTask(data.newTask);
+    }
 })
 
-.controller('AddTask',function($scope) {
-   
+.controller('AddTask',function($scope, Task) {
+    var data = {
+        newTask: ""
+    }
+    
+    function addTask() {
+        Task.addTask(data.newTask);
+        data.newTask = "";
+    }
+    
+    $scope.data = data;
+    $scope.addTask = addTask;
 })
--- www/js/services.js
+++ www/js/services.js
@@ -1,3 +1,23 @@
-angular.module('starter.services', []);
+angular.module('starter.services', [])
 
+
+.factory('Task', function() {
+    var data = {
+        tasks: []
+    };
+  
+  function addTask(title) {
+    data.tasks.push({title: title, added: new Date()});
+  }
+    
+  function deleteTask(title) {
+    data.taska.remove({title: title, added: new Date()});
+  }    
+    
+    
+    return {
+        data: data,
+        addTask: addTask
+    };
+});
 
